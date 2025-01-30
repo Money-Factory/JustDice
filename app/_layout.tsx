0d0e0d97221d0ae5515b98122e134c92c75b389e
Dyrk33 <20boyerd@gmail.com>
2025-01-29 20:01:24 -0800

    added navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,26 @@
 import { Slot } from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
 export default function RootLayout() {
   return (
-    <Slot />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Liar\'s Dice',
+            title: 'Liar\'s Dice',
+          }}
+        />
+        <Drawer.Screen
+          name="user/[id]" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'User',
+            title: 'overview',
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
