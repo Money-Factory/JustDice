c1e72a0cf2bae9173268367cfc0b8bcfe7be6ee6
Dyrk33 <20boyerd@gmail.com>
2025-02-25 19:00:04 -0800

    updated app.json with owner and version

--- app/purchase.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
-
-export default function Purchase() {
-    return (
-        <View>
-        </View>
-    );
-};
