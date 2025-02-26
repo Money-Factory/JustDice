86e60d350104fcb833dd2cd7dd4dda0ca52222bf
Dyrk33 <20boyerd@gmail.com>
2025-02-25 19:06:39 -0800

    Revert "Removes die.tsx"
    This reverts commit bd27d99ef9f8d558ee3dc7c2716c2e4d32a2f4a4.

---
+++ app/die.tsx
@@ -0,0 +1,35 @@
+import { View, TouchableOpacity } from "react-native";
+// import classNames from "classnames";
+// import './Die.css'
+
+export interface DieProps {
+    currentValue: number
+    isHeld: boolean
+}
+
+export default function Die(props: DieProps) {
+    const {currentValue, isHeld} = props
+    // const className = classNames({
+    //     held: isHeld,
+    //     notHeld: !isHeld
+    // })
+
+    // TODO - Do this through CSS
+    if (isHeld) {
+        return (
+            <View>
+                <div style={{width:"70px", height:"770px", border:"4px solid red"}}>
+                    {currentValue}
+                </div>
+            </View>
+        )
+    } else {
+        return (
+            <View>
+                <div style={{width:"70px", height:"70px", border:"4px solid black"}}>
+                    {currentValue}
+                </div>
+            </View>
+        )
+    }
+}
