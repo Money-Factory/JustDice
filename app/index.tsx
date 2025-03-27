69305901f6ab7efaee18d37d843734357fa8ce6a
Alex Waibel <alexwaibel@users.noreply.github.com>
2025-03-27 00:13:02 -0700

    Refactor liar's dice

--- app/index.tsx
+++ app/index.tsx
@@ -2,75 +2,61 @@ import { Text, View, StyleSheet, Button } from "react-native";
 import React from "react";
 import ShakeSensor from "../components/ShakeSensor";
 
-let one = "\u2680";
-let two = "\u2681";
-let three = "\u2682";
-let four = "\u2683";
-let five = "\u2684";
-let six = "\u2685";
-let pipArray = [one, two, three, four, five, six];
+const ONE = "\u2680";
+const TWO = "\u2681";
+const THREE = "\u2682";
+const FOUR = "\u2683";
+const FIVE = "\u2684";
+const SIX = "\u2685";
+const POSSIBLE_VALUES = [ONE, TWO, THREE, FOUR, FIVE, SIX];
 
 const randomNum = (min = 1, max = 6) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
-let diceCount = 6;
+
+const MAX_DIE_COUNT = 6;
+const MIN_DIE_COUNT = 0;
 
 export default function Index() {
-  const [firstDice, setFirstDice] = React.useState(one);
-  const [secondDice, setSecondDice] = React.useState(two);
-  const [thirdDice, setThirdDice] = React.useState(three);
-  const [fourthDice, setFourthDice] = React.useState(four);
-  const [fifthDice, setFifthDice] = React.useState(five);
-  const [sixthDice, setSixthDice] = React.useState(six);
-
+  const [diceCount, setDiceCount] = React.useState(MAX_DIE_COUNT);
+  const [diceValues, setDiceValues] = React.useState([...POSSIBLE_VALUES]);
   const [isRolling, setIsRolling] = React.useState(false);
 
-  const [firstDiceColor, setFirstDiceColor] = React.useState("black");
-  const [secondDiceColor, setSecondDiceColor] = React.useState("black");
-  const [thirdDiceColor, setThirdDiceColor] = React.useState("black");
-  const [fourthDiceColor, setFourthDiceColor] = React.useState("black");
-  const [fifthDiceColor, setFifthDiceColor] = React.useState("black");
-  const [sixthDiceColor, setSixthDiceColor] = React.useState("black");
+  const removeDie = () =>
+    setDiceCount(diceCount === MIN_DIE_COUNT ? MIN_DIE_COUNT : diceCount - 1);
+  const addDie = () =>
+    setDiceCount(diceCount === MAX_DIE_COUNT ? MAX_DIE_COUNT : diceCount + 1);
+
+  const roll = async () => {
+    setIsRolling(true);
+
+    for (let i = 0; i < 5; i++) {
+      setDiceValues(diceValues.map(() => POSSIBLE_VALUES[randomNum() - 1]));
+      await delay(100);
+    }
+
+    setIsRolling(false);
+  };
+
+  const delay = (durationMS: number) =>
+    new Promise((resolve) => setTimeout(resolve, durationMS));
 
   return (
     <View style={styles.main}>
       <View style={styles.topBar}>
         <View style={styles.subtractButton}>
-          <Button onPress={subtract} title="Remove Die" />
+          <Button onPress={removeDie} title="Remove Die" />
         </View>
         <View style={styles.addButton}>
-          <Button onPress={add} title="Add Die" />
+          <Button onPress={addDie} title="Add Die" />
         </View>
       </View>
 
       <ShakeSensor onShake={roll} threshold={4} cooldown={1000} />
 
       <View style={styles.diceSection}>
-        <View style={styles.diceRow}>
-          <Text style={{ fontSize: 175, color: firstDiceColor }}>
-            {firstDice}
-          </Text>
-          <Text style={{ fontSize: 175, color: secondDiceColor }}>
-            {secondDice}
-          </Text>
-        </View>
-
-        <View style={styles.diceRow}>
-          <Text style={{ fontSize: 175, color: thirdDiceColor }}>
-            {thirdDice}
-          </Text>
-          <Text style={{ fontSize: 175, color: fourthDiceColor }}>
-            {fourthDice}
-          </Text>
-        </View>
-
-        <View style={styles.diceRow}>
-          <Text style={{ fontSize: 175, color: fifthDiceColor }}>
-            {fifthDice}
-          </Text>
-          <Text style={{ fontSize: 175, color: sixthDiceColor }}>
-            {sixthDice}
-          </Text>
-        </View>
+        {Array.from({ length: diceCount }).map((_value, index) => {
+          return <Text style={styles.diceItem}>{diceValues[index]}</Text>;
+        })}
       </View>
 
       <View style={styles.bottomBar}>
@@ -80,102 +66,6 @@ export default function Index() {
       </View>
     </View>
   );
-
-  async function roll() {
-    setIsRolling(true);
-
-    setFirstDice(pipArray[randomNum() - 1]);
-    setSecondDice(pipArray[randomNum() - 1]);
-    setThirdDice(pipArray[randomNum() - 1]);
-    setFourthDice(pipArray[randomNum() - 1]);
-    setFifthDice(pipArray[randomNum() - 1]);
-    setSixthDice(pipArray[randomNum() - 1]);
-
-    await delay(100);
-
-    setFirstDice(pipArray[randomNum() - 1]);
-    setSecondDice(pipArray[randomNum() - 1]);
-    setThirdDice(pipArray[randomNum() - 1]);
-    setFourthDice(pipArray[randomNum() - 1]);
-    setFifthDice(pipArray[randomNum() - 1]);
-    setSixthDice(pipArray[randomNum() - 1]);
-
-    await delay(100);
-
-    setFirstDice(pipArray[randomNum() - 1]);
-    setSecondDice(pipArray[randomNum() - 1]);
-    setThirdDice(pipArray[randomNum() - 1]);
-    setFourthDice(pipArray[randomNum() - 1]);
-    setFifthDice(pipArray[randomNum() - 1]);
-    setSixthDice(pipArray[randomNum() - 1]);
-
-    await delay(100);
-
-    setFirstDice(pipArray[randomNum() - 1]);
-    setSecondDice(pipArray[randomNum() - 1]);
-    setThirdDice(pipArray[randomNum() - 1]);
-    setFourthDice(pipArray[randomNum() - 1]);
-    setFifthDice(pipArray[randomNum() - 1]);
-    setSixthDice(pipArray[randomNum() - 1]);
-
-    await delay(100);
-
-    setFirstDice(pipArray[randomNum() - 1]);
-    setSecondDice(pipArray[randomNum() - 1]);
-    setThirdDice(pipArray[randomNum() - 1]);
-    setFourthDice(pipArray[randomNum() - 1]);
-    setFifthDice(pipArray[randomNum() - 1]);
-    setSixthDice(pipArray[randomNum() - 1]);
-
-    setIsRolling(false);
-  }
-
-  function delay(durationMS: number) {
-    return new Promise((resolve) => setTimeout(resolve, durationMS));
-  }
-  function add() {
-    if (diceCount === 0) {
-      setFirstDiceColor("black");
-      diceCount++;
-    } else if (diceCount === 1) {
-      setSecondDiceColor("black");
-      diceCount++;
-    } else if (diceCount === 2) {
-      setThirdDiceColor("black");
-      diceCount++;
-    } else if (diceCount === 3) {
-      setFourthDiceColor("black");
-      diceCount++;
-    } else if (diceCount === 4) {
-      setFifthDiceColor("black");
-      diceCount++;
-    } else if (diceCount === 5) {
-      setSixthDiceColor("black");
-      diceCount++;
-    }
-  }
-
-  function subtract() {
-    if (diceCount === 6) {
-      setSixthDiceColor("white");
-      diceCount--;
-    } else if (diceCount === 5) {
-      setFifthDiceColor("white");
-      diceCount--;
-    } else if (diceCount === 4) {
-      setFourthDiceColor("white");
-      diceCount--;
-    } else if (diceCount === 3) {
-      setThirdDiceColor("white");
-      diceCount--;
-    } else if (diceCount === 2) {
-      setSecondDiceColor("white");
-      diceCount--;
-    } else if (diceCount === 1) {
-      setFirstDiceColor("white");
-      diceCount--;
-    }
-  }
 }
 
 const styles = StyleSheet.create({
@@ -184,16 +74,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: "100%",
   },
-  diceRow: {
-    flex: 1,
-    flexWrap: "wrap",
-    flexDirection: "row",
-    backgroundColor: "white",
-    justifyContent: "center",
-    height: "33%",
-  },
   diceSection: {
     height: "80%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  diceItem: {
+    width: "45%",
+    margin: "2.5%",
+    textAlign: "center",
+    fontSize: 175,
   },
   topBar: {
     height: "10%",
